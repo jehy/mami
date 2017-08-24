9d6da7ca432c2905c297dfa58f387bb171d59b18
Jehy <fate@jehy.ru>
2017-08-24 15:29:20 +0300

    update some labs and styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import '../css/prettify.css';
-import '../css/style.css';
 import '../../node_modules/code-prettify/styles/sunburst.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap-theme.css';
+import '../css/style.css';
 
 const $ = require('jquery');
 
